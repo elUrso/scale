56480c78db73a59ea303720392b9febbd27c8f90
Vitor Silva <sirmochi45@gmail.com>
2020-04-01 15:32:44 -0300

    Initial Commit

---
+++ scale/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  scale
+//
+//  Created by Vitor Silva on 01/04/20.
+//  Copyright © 2020 Vitor Silva. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
